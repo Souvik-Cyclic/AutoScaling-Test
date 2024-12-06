8a9374aecbad9b3cb11b657bbdd86f06a2cebf7d
Souvik-Cyclic <souvik.cyc@gmail.com>
2024-12-07 02:31:49 +0530

    creates a script to automate process

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+IMAGE_NAME="flask-app"
+
+echo "Pulling the latest changes from the repository"
+git pull origin main
+
+echo "Building the docker image"
+docker build -t $IMAGE_NAME .
+
+if [ "$(docker ps -aq --filter "name=$IMAGE_NAME")" ]; then
+    echo "Stopping and removing the existing container"
+    docker stop $(docker ps -aq --filter "name=$IMAGE_NAME")
+    docker rm $(docker ps -aq --filter "name=$IMAGE_NAME")
+else
+    echo "No existing container found"
+fi
+
+echo "Starting the container"
+docker run -d -p 5000:5000 --restart always --name $IMAGE_NAME $IMAGE_NAME
+echo "${IMAGE_NAME} is running on port 5000"
